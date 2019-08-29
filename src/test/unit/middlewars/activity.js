65be718cf57ef09ccc2936b65ed765e101023665
michaelost <mikhaylo.ostroviy@gmail.com>
2019-08-29 23:12:32 +0300

    cover activity middleware

--- src/test/unit/middlewars/activity.js
+++ src/test/unit/middlewars/activity.js
@@ -1,11 +1,40 @@
 const assert = require('assert');
 const sinon = require('sinon');
 const chai = require('chai');
+const jwt = require('jsonwebtoken');
+const { expect } = require('chai');
 
 const activityMiddleware = require('../../../middelware/activity');
 
 describe('Activity middleware', () => {
-  it('should be a function', function() {
-    assert.isFunction(activityMiddleware);
+  let req, res, next, jwtVerifySpy;
+
+  beforeEach(() => {
+    req = {
+      //body: {},
+      session: {},
+      headers: {},
+    };
+    res = {};
+    next = function () {};
+    jwtVerifySpy = sinon.spy(jwt, 'verify');
   });
+
+  afterEach(() => {
+    jwtVerifySpy.restore();
+  });
+
+  it('should verify token if token passed', function() {
+    req.body = { token: 'some token' };
+    activityMiddleware(req, res, next);
+    assert(jwtVerifySpy.called);
+  });
+
+  it('should not verify token if no token', function() {
+    activityMiddleware(req, res, next);
+    expect(jwtVerifySpy.called).to.be.false;
+  });
+
+
+
 });
